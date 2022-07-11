abb31112403b3f7b5cdeb6f8cfb20d959210cb17
Zineb106 <93834119+Zineb106@users.noreply.github.com>
2022-07-11 15:51:50 +0200

    Add files via upload

--- model-building/dashboard.py
+++ model-building/dashboard.py
@@ -1,5 +1,4 @@
 from datetime import timedelta
-from pathlib import Path
 from time import sleep
 
 import numpy as np
@@ -61,7 +60,6 @@ barplot_chart = st.write(fig1)
 fig55 = px.box(df_preprocessed, y="value" , points="all",  width=900, height=500)
 boxplot=st.write(fig55)
 
-from sklearn.neighbors import  LocalOutlierFactor
 
 
 #clf = LocalOutlierFactor(n_neighbors= 25, algorithm= 'brute', metric='manhattan')
@@ -114,62 +112,3 @@ boxplot_chart = st.plotly_chart(fig3)
 st.title('Modelization')
 
 # %% Modelization
-X = df_preprocessed[['temp', 'humidity']]
-model_rf = RandomForestRegressor(max_depth=2, n_estimators=10)
-model_rf.fit(X, y)	# %% Online timeseries
-
-def generate_new_row(df):
-    time_end_new_data = df['datetime'].max() + timedelta(hours=1)
-    random_number_temp = np.random.uniform(df['temp'].min(), df['temp'].max(),
-    	                                           size=(1), )
-    random_number_humidity = np.random.uniform(df['humidity'].min(), df['humidity'].max(),
-    	                                               size=(1), )
-    new_df = pd.DataFrame({'datetime': [time_end_new_data],
-    	                           'temp': random_number_temp,
-    	                           'humidity': random_number_humidity,
-    	                           'predicted': [True]})
-    return new_df
-
-def add_row(df, new_row_df):
-    return pd.concat([df, new_row_df], axis=0).reset_index(drop=True)
-
-def generate_new_prediction(df, row, model):
-    time_end_new_data = df['datetime'].max() + timedelta(hours=1)
-    X_pred = row[['temp', 'humidity']]
-    y_pred = model.predict(X_pred)
-    new_df = pd.DataFrame({'datetime': [time_end_new_data],
-    	                           'count': y_pred,'predicted': [True]})
-    return new_df
-	
-	
-def animate(df, column, chart):
-    
-    fig = px.line(df, x='datetime', y=column, color='predicted')
-    chart.plotly_chart(fig)
-    n_rows_to_display = 50
-    
-    df_for_predictions = df_preprocessed.copy()
-    df_for_predictions['predicted'] = False
-    fig = px.line(df_for_predictions.tail(n_rows_to_display), x='datetime', y='count', color='predicted')
-    online_ts_chart = st.plotly_chart(fig)
-    new_row_info = st.empty()
-    predicted_row_warning = st.empty()
-	
-    if st.sidebar.checkbox('Stream and predict on new data'):
-        bar = st.progress(0)
-    for i in range(11):
-    	        # get new row
-        new_row = generate_new_row(df_for_predictions)
-        new_row_info.info(f'Received new values: \n'f'temperature={np.round(new_row["temp"].values[0], 2)} - \n'f'humidity={np.round(new_row["humidity"].values[0], 2)} \n')
-           	        # predict
-        new_prediction = generate_new_prediction(df_for_predictions, new_row, model_rf)
-        predicted_row_warning.warning(f'Predicted count: {np.round(new_prediction["count"].values[0], 2)}')
-           	        # concatenate predicted row
-        df_for_predictions = add_row(df_for_predictions, new_prediction)
-        animate(df_for_predictions.tail(n_rows_to_display), 'count', online_ts_chart)
-        bar.progress(i * 10)
-    	        # wait
-        sleep(0.1)
-
-
-
