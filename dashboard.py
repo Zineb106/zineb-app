849f169f610d64b7d1068c1a484a20a782f689d9
Zineb106 <93834119+Zineb106@users.noreply.github.com>
2022-07-13 11:21:44 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -17,7 +17,7 @@ import datetime
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    st.write(df)
 else :
     def load_data():
     #bikes_data_path = 'bike_sharing_demand_train.csv'
